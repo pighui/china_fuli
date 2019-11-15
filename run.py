84b99087d1a119ea88ca60b50b4d851cbd95a337
xuhui <pighui233@163.com>
2019-11-15 14:56:02 +0800

    修改为每次运行都更新数据

--- run.py
+++ run.py
@@ -10,16 +10,16 @@ from datetime import date, datetime
 
 
 def update_data():
-    weekend = date(2019, 11, 10)
-    now = date.today()
-    week = (now - weekend).days % 7
-    hour = datetime.now().hour
-    # 每周2,4,7晚21:15开奖
-    if week in [2, 4, 0] and hour >= 22:
-        # 更新最新的开奖数据
-        chromosphere = Chromosphere()
-        print("更新了最新一期的数据")
-    print("暂无更新")
+    # weekend = date(2019, 11, 10)
+    # now = date.today()
+    # week = (now - weekend).days % 7
+    # hour = datetime.now().hour
+    # # 每周2,4,7晚21:15开奖
+    # if week in [2, 4, 0] and hour >= 22:
+    #     # 更新最新的开奖数据
+    chromosphere = Chromosphere()
+    #     print("更新了最新一期的数据")
+    print("数据已更新")
 def next_number():
     forecast = Forecast()
 
